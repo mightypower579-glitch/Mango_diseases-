d73a5b12e884d6cf49085374b87b712d2300fb10
mightypower579-glitch <mightypower579@gmail.com>
2026-07-27 21:53:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -220,7 +220,7 @@ model = load_model()
 
 st.markdown("""
 <div class="app-header">
-    <div class="app-title">GET 324 — Group C10</div>
+    <div class="app-title">GET 324 — Group CV1</div>
     <div class="app-subtitle">FruitScan</div>
     <div class="app-desc">formalin detection via deep learning — MobileNetV3</div>
 </div>
